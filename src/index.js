1132a40bd2b725afdea47d33359a543014dbd122
cristian.orozco <cristian.orozco@globant.com>
2021-07-05 09:55:18 -0500

    Expense tracker with voice interface

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {SpeechProvider} from '@speechly/react-client'
+
+import { Provider } from './context/context'
+import App from './App'
+import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+<SpeechProvider appId='201eb182-9bca-43be-b8e0-c73990b5190f' language='en-US'>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>
+    </SpeechProvider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
